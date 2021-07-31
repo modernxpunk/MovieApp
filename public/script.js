2ea1252b82460b1e7bf30e011c5fa0f2b29efb5a
modernxpunk <modernxpunk@gmail.com>
2021-07-31 17:17:56 +0300

    try to add smth

--- public/script.js
+++ public/script.js
@@ -4,6 +4,12 @@ const BASE_URL = "https://api.themoviedb.org/3"
 const API_KEY = "api_key=c1b842ef528e839d8688c74daa7f14d6"
 const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original"
 
+
+
+const BUTTON_POPULAR = `${BASE_URL}/discover/movie?${API_KEY}&sort_by=vote_average.desc&page=${activePage}`
+
+
+
 const API_URL = `${BASE_URL}/discover/movie?page=${activePage}&${API_KEY}`
 
 const films = document.querySelector('.films')
@@ -34,12 +40,17 @@ function addInnerCard(card, film) {
 			<div class="filmName"><p class="filmTitle">${film.title}</p></div>
 			<div class="score">
 				<div class="views">
-					<p class="vote_average">
-						${film.vote_average}
-						<img src="images/star.svg" style="width: 1em; margin: 2px;">
-					</p>
+					<span class="vote_average">
+						${film.vote_average}(${film.vote_count})
+						<img src="images/star.svg">
+					</span>
 				</div>
-				<div class="mark"><p class="vote_count">${film.vote_count}</p></div>
+				<div class="mark">
+					<span class="vote_count">
+						${film.popularity}
+						<img src="images/popularity.svg">
+					</span>
+				</div>
 			</div>
 		</div>
 	`
@@ -59,12 +70,14 @@ function editFilms(page, API) {
 	getMovie(API).then(data => {
 		const card = document.querySelectorAll('.card')
 		const films = data.results
+		const imgStar = `<img src="images/star.svg">`
+		const imgPopular = `<img src="images/popularity.svg">`
 		for (let i = 0; i < films.length; i++) {
 			const film = films[i]
 			card[i].querySelector('.backgroundImage').style.backgroundImage = `url(${BASE_IMAGE_URL + film.poster_path})`
 			card[i].querySelector('.filmTitle').innerHTML = film.title
-			card[i].querySelector('.vote_average').innerHTML = film.vote_average
-			card[i].querySelector('.vote_count').innerHTML = film.vote_count
+			card[i].querySelector('.vote_average').innerHTML = film.vote_average + imgStar
+			card[i].querySelector('.vote_count').innerHTML = film.vote_count + imgPopular
 		}
 	})
 }
@@ -111,20 +124,23 @@ function changePage(pages, activePage) {
 
 function howChange(change, curPage) {
 	activePage = change !== 0 ? activePage + change : curPage
-	const API = `${BASE_URL}/discover/movie?page=${activePage}&${API_KEY}`
 
 	removeColor(pages)
 	changePage(pages, activePage)
 	addColor(pages)
+
+	let API = `${BASE_URL}/discover/movie?page=${activePage}&${API_KEY}`
+
 	editFilms(activePage, API)
 }
+
 
 previous_button.addEventListener('click', () => {
 	if (activePage !== 1)
 		howChange(-1)
 })
 
-paginationPages.addEventListener('click', (e) => {
+paginationPages.addEventListener('click', e => {
 	const curPage = Number(e.target.innerHTML)
 	if (!isNaN(curPage) && curPage !== activePage)
 		howChange(0, curPage)
