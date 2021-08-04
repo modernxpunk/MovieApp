134cfada1184c5ef1f2230941fec1fc86bb8aec5
modernxpunk <modernxpunk@gmail.com>
2021-08-04 17:48:10 +0300

    add page with film info

--- public/script.js
+++ public/script.js
@@ -4,21 +4,23 @@ const BASE_URL = "https://api.themoviedb.org/3"
 const API_KEY = "api_key=c1b842ef528e839d8688c74daa7f14d6"
 const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original"
 
-
-
 const BUTTON_POPULAR = `${BASE_URL}/discover/movie?${API_KEY}&sort_by=vote_average.desc&page=${activePage}`
 
-
-
 const API_URL = `${BASE_URL}/discover/movie?page=${activePage}&${API_KEY}`
-
+const getMovie = async url => await fetch(url).then(res => res.json())
 const films = document.querySelector('.films')
+
+getMovie(API_URL).then(data => {
+	for (film of data.results) {
+		films.append(addInnerCard(createNode('card'), film))
+	}
+})
+
 const pages = document.querySelectorAll('.page')
 const next_button = document.querySelector('.next_button')
 const paginationPages = document.querySelector('.pages')
 const previous_button = document.querySelector('.previous_button')
 
-const getMovie = async url => await fetch(url).then(res => res.json())
 
 function createNode(name) {
 	const node = document.createElement('div')
@@ -27,6 +29,7 @@ function createNode(name) {
 }
 
 function addInnerCard(card, film) {
+	card.id = film.id
 	card.innerHTML += `
 		<div
 			class="backgroundImage"
@@ -42,14 +45,14 @@ function addInnerCard(card, film) {
 				<div class="views">
 					<span class="vote_average">
 						${film.vote_average}(${film.vote_count})
-						<img src="images/star.svg">
 					</span>
+					<img src="images/star.svg">
 				</div>
 				<div class="mark">
 					<span class="vote_count">
 						${film.popularity}
-						<img src="images/popularity.svg">
 					</span>
+					<img src="images/popularity.svg">
 				</div>
 			</div>
 		</div>
@@ -57,27 +60,20 @@ function addInnerCard(card, film) {
 	return card 
 }
 
-function readFilms(page) {
-	getMovie(API_URL).then(data => {
-		for (film of data.results) {
-			films.append(addInnerCard(createNode('card'), film))
-		}
-	})
-}
-readFilms(activePage)
-
 function editFilms(page, API) {
 	getMovie(API).then(data => {
 		const card = document.querySelectorAll('.card')
 		const films = data.results
-		const imgStar = `<img src="images/star.svg">`
-		const imgPopular = `<img src="images/popularity.svg">`
-		for (let i = 0; i < films.length; i++) {
+		for (let i = 0; i < 20; i++) {
 			const film = films[i]
-			card[i].querySelector('.backgroundImage').style.backgroundImage = `url(${BASE_IMAGE_URL + film.poster_path})`
-			card[i].querySelector('.filmTitle').innerHTML = film.title
-			card[i].querySelector('.vote_average').innerHTML = film.vote_average + imgStar
-			card[i].querySelector('.vote_count').innerHTML = film.vote_count + imgPopular
+			if (film) {
+				card[i].id = film.id
+				card[i].querySelector('.backgroundImage').style.backgroundImage = `url(${BASE_IMAGE_URL + film.poster_path})`
+				card[i].querySelector('.filmTitle').innerHTML = film.title
+				card[i].querySelector('.vote_average').innerHTML = `${film.vote_average}(${film.vote_count})`
+			} else {
+				card[i].innerHTML = ''
+			}
 		}
 	})
 }
@@ -94,7 +90,7 @@ function addColor(pages) {
 			page.classList.add('active')
 }
 
-function changePage(pages, activePage) {
+function showPages(pages, activePage) {
 	if (activePage < 5) {
 		pages[0].innerHTML = 1
 		pages[1].innerHTML = 2
@@ -122,16 +118,19 @@ function changePage(pages, activePage) {
 	}
 }
 
-function howChange(change, curPage) {
-	activePage = change !== 0 ? activePage + change : curPage
+function changeAPIpage(API, curentPage) {
+	let numberPage = +API.slice(API.indexOf('page=') + 5, API.indexOf('&'))
+	return API.replace(`page=${numberPage}`, `page=${curentPage}`)
+}
+
+function howChange(change, currentPage=activePage) {
+	activePage = change !== 0 ? activePage + change : currentPage
 
 	removeColor(pages)
-	changePage(pages, activePage)
+	showPages(pages, activePage)
 	addColor(pages)
 
-	let API = `${BASE_URL}/discover/movie?page=${activePage}&${API_KEY}`
-
-	editFilms(activePage, API)
+	editFilms(activePage, changeAPIpage(API_URL, activePage))
 }
 
 
@@ -150,3 +149,77 @@ next_button.addEventListener('click', () => {
 	if (activePage !== 500)
 		howChange(1)
 })
+
+const pagination = document.querySelector('.pagination')
+const header = document.querySelector('.header')
+const currentFilm = document.querySelector('.film')
+const back = document.querySelector('.back')
+
+
+const defaultFilm = currentFilm.innerHTML
+
+
+const copy = text => {
+	const info = document.createElement('p')
+	info.innerText = text
+	return info
+}
+
+films.addEventListener('click', e => {
+	currentFilm.innerHTML = defaultFilm
+
+	document.querySelector('.back').style.display = 'block'
+	header.style.display = 'none'
+	films.style.display = 'none'
+	pagination.style.display = 'none'
+
+	const ID_FILM = e.target.closest('.card').id
+
+	const filmAPI = `${BASE_URL}/movie/${ID_FILM}?${API_KEY}`
+
+	getMovie(filmAPI).then(data => {
+
+		const film_image = document.querySelector('.film_image')
+		const film_production_companies = document.querySelector('.film_production_companies')
+		const film_production_countries = document.querySelector('.film_production_countries')
+		const film_release_date = document.querySelector('.film_release_date')
+		const film_status = document.querySelector('.film_status')
+		const film_full_name = document.querySelector('.film_full_name')
+		const film_rating = document.querySelector('.film_rating')
+		const film_voting = document.querySelector('.film_voting')
+		const film_desc = document.querySelector('.film_desc')
+		const film_genres = document.querySelector('.film_genres')
+
+		film_image.src = `${BASE_IMAGE_URL + data.poster_path}`
+
+		for (production_companies of data.production_companies)
+			film_production_companies.innerHTML += `<p>${production_companies.name}</p>`
+
+		for (production_countries of data.production_countries) {
+			film_production_countries.innerHTML += `<p>${production_countries.name}</p>`
+		}
+
+		film_release_date.append(copy(data.release_date))
+		film_status.append(copy(data.status))
+		film_full_name.innerHTML = data.title
+		film_rating.innerHTML = data.vote_average
+		film_voting.innerHTML = data.vote_count
+		film_desc.innerHTML = data.overview
+
+		for (genre of data.genres)
+			film_genres.innerHTML += `<div class="genre"><p>${genre.name}</p></div>`
+
+		currentFilm.style.display = 'flex'
+	})
+
+	document.querySelector('.back').addEventListener('click', () => {
+		back.style.display = 'none'
+		pagination.style.display = 'flex'
+		header.style.display = 'flex'
+		films.style.display = 'flex'
+		currentFilm.style.display = 'flex'
+		document.querySelector('.film').style.display = 'none'
+	})
+})
+
+
